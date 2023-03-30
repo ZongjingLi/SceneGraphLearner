41e15268768268b5c4348cd492a9527c2fa1b5b6
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-03-31 06:40:43 +0800

    add the config and start to train the scene graph.

---
+++ config.py
@@ -0,0 +1,13 @@
+import torch
+import argparse 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--name",   default = "SceneGraphLearner")
+
+# setup the perception module
+parser.add_argument("--hidden_dim",default = 100)
+
+# setup the concept learner 
+parser.add_argument("--concept_dim",default = 100)
+
+config = parser.parse_args(args = [])
