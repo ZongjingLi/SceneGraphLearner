68b8511cfe187fa951b3427144f3e063138de3d9
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-03-31 07:15:48 +0800

    train the ptr dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,3 +2,4 @@ from .clevr4 import *
 from .sprites import *
 from .partnet import *
 from .toy import *
+from .ptr import *
---
+++ train.py
@@ -0,0 +1,26 @@
+import torch
+import argparse 
+
+from datasets.ptr import *
+
+from config import *
+
+def train(model, args):
+    print("\nstart the experiment: {}".format(args.name))
+    print("experiment config: \nepoch: {} \nbatch: {} samples \nlr: {}\n".format(args.epoch,args.batch_size,args.lr))
+    
+    if args.dataset == "ptr":
+        train_dataset = PTRData()
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument("--name",            default = "KFT")
+argparser.add_argument("--epoch",           default = 3000)
+argparser.add_argument("--lr",              default = 2e-4)
+argparser.add_argument("--batch_size",      default = 4)
+argparser.add_argument("--dataset",         default = "ptr")
+
+args = argparser.parse_args(args = [])
+
+
+train(0, args)
+
