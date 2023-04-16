b4011d3e6cb3fec6396742600c102719d312a960
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-04-17 07:48:01 +0800

    update the acherus

--- scripts/train_toy_archerus_joint.sh
+++ scripts/train_toy_archerus_joint.sh
@@ -1,1 +1,1 @@
-/Users/melkor/miniforge3/envs/Melkor/bin/python train.py --name="Acherus" --training_mode="joint"
+/Users/melkor/miniforge3/envs/Melkor/bin/python train.py --name="Acherus" --training_mode="joint" --pretrain_perception="checkpoints/Acherus_perception_toy_psgnet.ckpt"
