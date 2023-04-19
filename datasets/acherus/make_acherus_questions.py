9320ef32aaeec4dff730a1f7bdd04523650883c4
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-04-19 15:12:41 +0800

    welcome to the acherus

--- datasets/acherus/make_acherus_questions.py
+++ datasets/acherus/make_acherus_questions.py
@@ -1,10 +1,29 @@
-from utils import save_json
+import json
+
+def save_json(data,path):
+    '''input the diction data and save it'''
+    beta_file = json.dumps(data)
+    file = open(path,'w')
+    file.write(beta_file)
+    return True
+
+all_questions = []
 
 for i in range(130):
-    question_bind = {}
+    questions_answer_pairs = []
     if i <= 83:
-        pass
         answer = True
     else:
-        pass
-        answer = False
+        answer = "no"
+    questions_answer_pairs.append(
+                {
+            "question":"is there any house in the scene",
+            "program":"exist(filter(scene(),house))",
+            "answer":answer
+                }
+            )
+    all_questions.append(questions_answer_pairs)
+
+root = "/Users/melkor/Documents/datasets/"
+
+save_json(all_questions, root + "acherus/" + "train_questions.json")
