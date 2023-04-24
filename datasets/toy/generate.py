d208b881b746c1f50d5da9b42c4b0d185d92a548
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-04-25 02:50:25 +0800

    generate a better dataset

--- datasets/toy/generate.py
+++ datasets/toy/generate.py
@@ -48,7 +48,7 @@ def random_template():
 
 def random_color():
     color = [0, 10, 0]
-    color[np.random.choice([0,2])] = 200
+    color[np.random.choice([0,2])] = 150 + np.random.randint(-50,60)
     return color
 
 def random_coord(margin,resolution = (128,128)):
