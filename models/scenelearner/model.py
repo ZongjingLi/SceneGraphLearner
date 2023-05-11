8a65323adda300361f9728aab23e627b8437e6f6
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-05-11 11:26:52 +0800

    change the code to the Valkyr setup

--- models/scenelearner/model.py
+++ models/scenelearner/model.py
@@ -18,6 +18,8 @@ class SceneLearner(nn.Module):
         # [Unsupervised Part-Centric Representation]
         if config.perception == "psgnet":
             self.scene_perception = SceneGraphNet(config)
+        if config.perception == "local_psgnet":
+            self.scene_perception = LocalSceneGraphNet(config)
         if config.perception == "slot_attention":
             self.scene_perception = SlotAttentionParser(config.object_num, config.object_dim,5)
             self.part_perception = SlotAttention(config.part_num, config.object_dim,5)
--- scripts/valkyr_perception.sh
+++ scripts/valkyr_perception.sh
@@ -1,2 +1,1 @@
-/Users/melkor/miniforge3/envs/Melkor/bin/python train.py
- --name="Valkyr" --training_mode="perception"
+/Users/melkor/miniforge3/envs/Melkor/bin/python train.py --name="Valkyr" --training_mode="perception"
