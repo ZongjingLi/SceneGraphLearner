48da995b2a59afbf519b843fb6177211ea4edcbe
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-05-18 15:43:23 +0800

    Update cstnet.py

--- models/percept/stnet/cstnet.py
+++ models/percept/stnet/cstnet.py
@@ -1,1 +1,63 @@
-import torch
+from stnet import *
+
+class ControlPSGNet(torch.nn.Module):
+    def __init__(self,imsize, perception_size, node_feat_size, struct = [1, 1]):
+
+        super().__init__()
+        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        
+        self.device = device
+        self.imsize = imsize
+
+        num_graph_layers = 2
+
+        self.spatial_edges,self.spatial_coords = grid(imsize,imsize,device=device)
+        
+        # [Global Coords]
+        self.global_coords = self.spatial_coords.float()
+
+        self.spatial_edges = build_perception(imsize,perception_size,device = device)
+        self.spatial_coords = self.spatial_coords.to(device).float() / imsize
+        # Conv. feature extractor to map pixels to feature vectors
+        self.rdn = RDN(SimpleNamespace(G0=node_feat_size  ,RDNkSize=3,n_colors=3,
+                               RDNconfig=(4,3,16),scale=[2],no_upsampling=True))
+
+
+
+        # Affinity modules: for now just one of P1 and P2 
+        self.affinity_aggregations = torch.nn.ModuleList([])
+        for s in struct:
+            if s == 1: self.affinity_aggregations.append(P1AffinityAggregation())
+            if s == 2: self.affinity_aggregations.append(P2AffinityAggregation())
+        
+        self.control_aggregations = torch.nn.ModuleList([])
+        control = [10,4]
+        for i in control:
+            
+
+        # Node transforms: function applied on aggregated node vectors
+
+        self.node_transforms = torch.nn.ModuleList([
+            FCBlock(hidden_ch=100,
+                    num_hidden_layers=3,
+                    in_features =node_feat_size + 4,
+                    out_features=node_feat_size,
+                    outermost_linear=True) for _ in range(len(self.affinity_aggregations))
+        ])
+
+        # Graph convolutional layers to apply after each graph coarsening
+        gcv = GraphConv(node_feat_size, node_feat_size)  
+        self.graph_convs = torch.nn.ModuleList([
+            GraphConv(node_feat_size , node_feat_size ,aggr = "mean")   for _ in range(len(self.affinity_aggregations))
+        ])
+
+        # Maps cluster vector to constant pixel color
+        self.node_to_rgb  = FCBlock(hidden_ch=100,
+                                    num_hidden_layers=3,
+                                    in_features =20,
+                                    out_features=3,
+                                    outermost_linear=True)
+
+        self.node_to_qtr_p1  = FCBlock(100,3,node_feat_size,6 * 3,outermost_linear = True)
+        self.node_to_qtr_p2  = FCBlock(100,3,node_feat_size,6 * 3,outermost_linear = True)
+        self.gauge = nn.Linear(node_feat_size,node_feat_size)
