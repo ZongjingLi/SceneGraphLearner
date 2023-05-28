e90ba3812a15f797f7c20282a3ba8189f2d4627f
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-05-28 17:53:44 +0800

    particle filter, derender, matcher and the step

---
+++ models/physics/particle_filter.py
@@ -0,0 +1,56 @@
+import random
+import time
+import os
+
+import logging
+from copy import deepcopy, copy
+from statistics import mean
+
+import pycocotools.mask as mask_util
+from multiprocessing import Pool, cpu_count
+from tqdm import tqdm
+
+import pybullet as p
+import pybullet_data
+import numpy as np
+
+from utils import * 
+# TODO: IOU geometry import
+
+class _ParticleUpdater(object):
+    def __init__(self, config, t, belief, camera, observation_history = None):
+        self.camera = camera
+        self.belief = deepcopy(belief)
+
+        if observation_history is None:
+            self.observation_history = [deepcopy(belief)]
+        else:
+            self.observation_history = copy(observation_history)
+        
+        self.n_objects = 0
+
+        # TODO: Matcher and Stepper
+        self.matcher = Matcher(config)
+        self.stepper = Stepper(config)
+
+        self.t = t
+        self.area_threhold = config.area_threshold
+        #TODO: config.area_threshold
+
+        samples_mass = config.mass
+        #TODO: config.mass
+
+        self.to_sample_mass = config.to_sample_mass
+        #TODO: to sample mass
+
+    
+    def step(self):
+        "Belief Step"
+        self.belief, magic_penalty = self.stepper.step(self.belief)
+        self.magic_penalty += magic_penalty
+        self.t += 1
+
+class FilterUpdate(object):
+    def __init__(self, config, belief, case_name, camera, n_filter):
+        self.t = 0
+        self.config = config
