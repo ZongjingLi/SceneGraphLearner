6f31b7eb2bef37f1bf5bd0fc6614bf9780ab8af0
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-05-30 18:12:04 +0800

    angrathar

--- config.py
+++ config.py
@@ -22,10 +22,10 @@ parser.add_argument("--object_num",         default = 5)
 parser.add_argument("--part_num",           default = 3)
 
 # setup the concept learner 
-parser.add_argument("--concept_type",       default = "box")
+parser.add_argument("--concept_type",       default = "cone")
 parser.add_argument("--concept_dim",        default = 102)
 parser.add_argument("--object_dim",         default = 102)
-parser.add_argument("--temperature",        default = 5)
+parser.add_argument("--temperature",        default = 0.2)
 
 # box concept methods
 parser.add_argument("--method",             default = "uniform")
