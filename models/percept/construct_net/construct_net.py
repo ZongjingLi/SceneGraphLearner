a58500940417b9942e6e45a340510ebc7c779459
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-06-15 22:45:08 +0800

    add the graph convolution layer

--- models/percept/construct_net/construct_net.py
+++ models/percept/construct_net/construct_net.py
@@ -2,5 +2,23 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from .convs import *
 from .projection import *
 from .graph_propagation import *
+
+class ConstructNet(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        # [Convolution on the Image Grid]
+        self.grid_conv = 0
+
+    def forward(self, x):
+        return x
+
+class ConstuctLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, nodes, edge_affinities):
+        pass
