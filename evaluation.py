61e27073ca95f5df5239ea52c041c451e085c748
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-07-27 18:27:48 +0800

    ds

--- evaluation.py
+++ evaluation.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 
 import argparse
+from datasets import *
 from config import *
 
 evalparser = argparse.ArgumentParser()
@@ -11,9 +12,20 @@ evalparser.add_argument("--checkpoint_dir", default = False)
 evalparser.add_argument("--")
 evalargs = evalparser.parse_args(args = [])
 
-def evaluate_pointcloud(evalmodel, config, args):
+def evaluate_pointcloud(evalmodel, config, args, phase = "0"):
     evalmodel = evalmodel.to(args.device)
-    pass
+    assert phase in ["0", "1", "2", "3", "4", "5",0,1,2,3,4,5],print("not a valid phase")
+    query = True if args.training_mode in ["joint", "query"] else False
+
+    if args.dataset == "Objects3d":
+        eval_dataset= Objects3dDataset(config, sidelength = 128, stage = int(phase))
+
+    evalloader = DataLoader(eval_dataset)
+    for sample in evalloader:
+        inputs = sample
+        outputs = evalmodel(inputs)
+
+    print("evaluation process done!")
 
 if evalargs.checkpoint_dir:
     model = model(config)
