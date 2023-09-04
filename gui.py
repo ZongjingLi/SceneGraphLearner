d4a6374062a367b29e2f4a9c26131b8822b9fd0e
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-09-04 20:45:47 +0800

    add some render utils

--- gui.py
+++ gui.py
@@ -10,19 +10,14 @@ import taichi as ti
 ti.init(arch = ti.cpu)
 
 
-def wrong_func(x):return x
-
-@ti.func
-def inv_square(x):return x
-
-@ti.kernel
-def partial_sum(n: int) -> float:
-    total = inv_square(n)
-    return total
-
+# [Claim some Basic Types]
 vec3d = ti.types.vector(3, ti.f64)
 vec4d = ti.types.vector(4, ti.f64)
 mat4x3i = ti.types.matrix(4,3,dtype = ti.f64)
+
+@ti.func
+def inv_square(x):return 1.0 / (x * x)
+
 
 @ti.func
 def norm(w: vec4d):
@@ -33,22 +28,30 @@ def test(v: vec4d) -> ti.types.f64:
     outputs = norm(v)
     return outputs
 
+Sphere = ti.types.struct(center= vec3d, radius = float)
+
+
+@ti.func
+def sphere_volume(s: Sphere):
+    return s.radius  ** 3
+
+@ti.kernel
+def evaluate_run(s: Sphere) -> float:
+    return sphere_volume(s)
+
 v = vec4d(1,2,0,3)
 v_norm = test(v)
 print("v_norm:",v_norm)
-
-@dataclass
-class Sphere:
-    center: vec3d
-    radius: float
 
 center = vec3d(1,0,1)
 radius = 2.0
 sphere1 = Sphere(center, radius)
 print(sphere1)
+print(evaluate_run(sphere1))
 
 W, H = (640,640)
 random_image = (torch.randn(W,H,3) ** 2).clamp(0.0,1.0).numpy()
+
 
 main_gui = ti.GUI("Main GUI", (W,H) )
 while main_gui.running:
---
+++ utils/render.py
@@ -0,0 +1,37 @@
+
+import torch
+import torch.nn as nn
+
+def get_rays(H, W, K, c2w, inverse_y, flip_x, flip_y, mode = "center"):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    i, j = torch.meshgrid(\
+        torch.linspace(0,W-1, W, device = device),
+        torch.linspace(0,H-1, H, device = device))
+    i = i.t().float()
+    j = j.t().float()
+    if mode == "lefttop":
+        pass
+    elif mode == "center":
+        i, j = i + 0.5, j + 0.5
+    else:
+        raise NotImplementedError
+    if flip_x: i = i.flip((1,))
+    if flip_y: j = j.flip((0,))
+
+    if inverse_y:
+        dirs = torch.stack([
+            (i - K[0][2])/K[0][0],
+            (j - K[1][2])/K[1][1],
+            torch.ones_like(i)
+        ], dim = -1)
+    else:
+        dirs = torch.stack([
+            (i - K[0][2])/K[0][0],
+            -(j - K[1][2])/K[1][1],
+            -torch.ones_like(i)
+        ], dim = -1)
+    # Rotate ray direction from camera frame to the world frame
+    rays_d = torch.sum()
+    # Translate the camerta's frame to the world frame.
+    rays_o = c2w[:3,3]
+    return rays_o, rays_d
